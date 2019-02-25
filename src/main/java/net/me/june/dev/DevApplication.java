0ab51e0c9d139299716542482718780f003b4b84
june <pupupee9@gmail.com>
2019-02-25 22:40:28 +0900

    main page posts 추가

--- src/main/java/net/me/june/dev/DevApplication.java
+++ src/main/java/net/me/june/dev/DevApplication.java
@@ -1,9 +1,13 @@
 package net.me.june.dev;
 
+import net.me.june.dev.domain.PostDTO;
+import net.me.june.dev.serivice.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
@@ -11,8 +15,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DevApplication {
 
+    @Autowired
+    private PostService postService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(PostDTO postDTO,Model model) {
+        model.addAttribute("posts",postService.findBoards(postDTO));
         return "index";
     }
 
